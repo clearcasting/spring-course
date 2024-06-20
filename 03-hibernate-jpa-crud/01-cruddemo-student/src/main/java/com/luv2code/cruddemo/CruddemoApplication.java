e0e4a984320413f2bc645f1ec29f9da9e88d5814
clearcasting <lateralcreativity@outlook.com>
2024-06-20 13:55:37 -0500

    reading objects done

--- 03-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -21,9 +21,31 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
 //			createStudent(studentDAO);
+//			createMultipleStudents(studentDAO);
 
-			createMultipleStudents(studentDAO);
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		// create a student object
+		System.out.println("Creating new student object ...");
+		Student student = new Student("Lee", "Sin", "lol@riot.com");
+
+		// save the student
+		System.out.println("Saving the student ...");
+		studentDAO.save(student);
+
+		// display id of the saved student
+		int studentId = student.getId();
+		System.out.println("Saved student. Generated id: " + studentId);
+
+		// retrieve student based on the id: primary key
+		System.out.println("Retrieving student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+
+		// display student
+		System.out.println("Found the student: " + myStudent);
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
