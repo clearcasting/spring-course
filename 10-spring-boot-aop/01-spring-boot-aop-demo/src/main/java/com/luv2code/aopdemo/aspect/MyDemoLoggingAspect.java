60d4a5877743dbd0420cd1cf9dc48a7616adb651
clearcasting <lateralcreativity@outlook.com>
2024-06-28 09:47:10 -0500

    pointcut expressions done

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,14 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-    // specify path to run only on specific method
-//    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
-
-    // use wildcard * to match any method matching everything to the left of the *
-//    @Before("execution(public void add*())")
-
-    // modifier is optional, can use wildcard to match any return type
-    @Before("execution(* add*())")
+    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
     }
